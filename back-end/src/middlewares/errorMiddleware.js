8e2197c375564a7314bd4d0ab4c984bcd6a181fa
mtaher36 <mtaher3331@gmail.com>
2024-06-05 16:19:41 +0700

    create api auth

---
+++ back-end/src/middlewares/errorMiddleware.js
@@ -0,0 +1,8 @@
+import logger from '../config/logger.js';
+
+const errorMiddleware = (err, req, res, next) => {
+  logger.error(err.stack);
+  res.status(500).json({ error: 'Internal server error' });
+};
+
+export default errorMiddleware;
