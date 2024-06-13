1512d52e4176f6a734302a4a96c7058ad0b3151a
fauqaji <133103837+fauqaji@users.noreply.github.com>
2024-06-13 17:30:39 +0700

    commit
    fix

--- front-end/src/components/Headers/HeaderStats.js
+++ front-end/src/components/Headers/HeaderStats.js
@@ -8,13 +8,11 @@ export default function HeaderStats() {
   return (
     <>
       {/* Header */}
-      <div className="relative bg-lightBlue-600 md:pt-32 pb-32 pt-12">
+      <div className="relative bg-blueGray-100 md:pt-10 pb-32 pt-5">
         <div className="px-4 md:px-10 mx-auto w-full">
           <div>
             {/* Card stats */}
-            <div className="flex flex-wrap">
-              
-            </div>
+            <div className="flex flex-wrap"></div>
           </div>
         </div>
       </div>
